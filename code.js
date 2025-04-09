74312777b0cb56fb8b5f00e87096c391202797dc
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-04-09 15:47:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,17 @@
-function augmentingPath(graph, start, end) {
+function augmentingPath(graph, start, end, visited = new Set()) {
+    if (start == end) return [start];
+    
+    visited.add(start);
+    
+    for (let i in graph[start]) {
+        if (!visited.has(i) && graph[start][i] > 0) {
+
+            let augmentedPath = augmentingPath(graph, i, end, visited);
+            if (augmentedPath.length > 0) {
+                return [start, ...augmentedPath];
+            }
+        }
+    }
+    
     return [];
 }
